c5403382267dfa0b8f8386bcc38671111e3cbb13
azurata <azurata@vivaldi.net>
2019-12-01 13:03:26 +0900

    add scenario

--- main.js
+++ main.js
@@ -1,57 +1,71 @@
-Vue.component('messagebox', {
-  props: ['talker', 'message'],
+Vue.component("messagebox", {
+  props: ["talker", "message"],
   template: `
     <div id="messagebox">
-      <div id="talker">{{ talker }}</div>
+      <div id="talker">{{ talker }}</div><hr>
       <div id="message">{{ message }}</div>
     </div>
-  `,
-});
-
-Vue.component('selectbox', {
-  props: ['item1', 'item2'],
-  template: `
-    <div id="selectbox">
-      <div class="item">{{ item1 }}</div>
-      <div class="item">{{ item2 }}</div>
-    </div>
-  `,
+  `
 });
 
 var vm = new Vue({
   el: "#app",
   data: {
-    section: 2,
+    section: 0,
     talker: "",
     message: "",
     index: 0,
     selecting: false,
-    item1: '',
-    item2: '',
+    item1: "",
+    item2: "",
   },
-  created: function () {
+  created: function() {
     this.talker = scenario[this.section].talks[this.index].talker;
     this.message = scenario[this.section].talks[this.index].content;
   },
   methods: {
-    onTap: function () {
-      this.index++;
+    indexNext: function() {
+      if (!this.selecting && scenario[this.section].talks.length > this.index)
+        this.index++;
     },
-    changeSection: function (newSection) {
+    changeSection: function(newSection) {
       this.section = newSection;
       this.index = 0;
     },
-  },
-  watch: {
-    index: function (newIndex, oldIndex) {
-      if(newIndex >= scenario[this.section].talks.length)
+    selectItem1: function() {
+      switch(this.section)
       {
-        this.index = oldIndex;
-      }
-      else {
-        this.talker = scenario[this.section].talks[this.index].talker;
-        this.message = scenario[this.section].talks[this.index].content;
+        case 0:
+          this.changeSection(1);
+          break;
+        case 2:
+          this.changeSection(3);
+          break;
       }
     },
+    selectItem2: function() {
+      switch(this.section)
+      {
+        case 0:
+          this.changeSection(2);
+          break;
+        case 2:
+          this.changeSection(4);
+          break;
+      }
+    },
+  },
+  watch: {
+    index: function(newIndex, oldIndex) {
+      if (scenario[this.section].talks[this.index].talker != "if") {
+        this.selecting = false;
+        this.talker = scenario[this.section].talks[this.index].talker;
+        this.message = scenario[this.section].talks[this.index].content;
+      } else {
+        this.selecting = true;
+        this.item1 = scenario[this.section].talks[this.index].content[0];
+        this.item2 = scenario[this.section].talks[this.index].content[1];
+      }
+    }
   }
 });
